e6fa3413a6d455d397103f6a2ee2edba43e4fe43
noah White <nwhite30@knights.ucf.edu>
2022-01-09 13:53:56 -0500

    Remove file from repository

--- cv_test.py
+++ cv_test.py
@@ -1,6 +1,21 @@
 import cv2 as cv
 import numpy as np
+from board import board
+import matplotlib.pyplot as plt
+import computer_vision
 
-img = cv.imread('pictures/goose.jpg')
-cv.imshow('image', img)
-cv.waitKey(0)
+boardImg = cv.imread('pictures/prev_2.jpg')
+boardImg2 = cv.imread('pictures/frame.jpg')
+imgPlot = plt.imshow(boardImg2)
+plt.show()
+myBoard = board(boardImg2)
+
+square = myBoard.getSquare([100,400])
+print(f'square: {square}')
+
+# computer_vision.drawBlobPoints(boardImg, 7)
+# cv.waitKey(0)
+# computer_vision.drawBlobPoints(boardImg2, 7)
+# #square = myBoard.getSquare([660,60])
+# cv.waitKey(0)
+
