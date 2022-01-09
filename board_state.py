3cbb8fa89f4543236ad6efd57acf7a84e0fd0a98
noah White <nwhite30@knights.ucf.edu>
2022-01-09 13:03:21 -0500

    Change return type of getOccupiedSquares to 1D array
    getOccupiedSquares now just returns a list of squares at which the whitePieceColor occurs because we are now only concerned with tracking the white pieces

--- board_state.py
+++ board_state.py
@@ -22,61 +22,41 @@ class boardState:
         pass
         
 
-
+    # Method returns a list of squares occupied by white pieces
+    # This is frequently returning an aray with two 'None' values for some reason
     def getOccupiedSquares(self):
-        # Lists of colors will always be in ROYGBIV format
-        # Lists of pieces will always be in PNBRQK format
 
-        # A list of the colors for each piece in the aformentioned order
-        # Each unique piece will be one of 6 colors, the remaining color will be used to outline the corners of the board
-        pieceColors = [0,1,2,3,4,5]
+        whitePieceColor = 7
 
-        # Pawn - Red
-        # Knight - Orange
-        # Bishop - Yellow
-        # Rook - Green
-        # Queen - Cyan
-        # King - Indigo
+
+
+        # None of this is necessary as we only need to track the white pieces
+        # Pieces also do not need to be distinguished from one another in any way
         
+
 
         # A 2D list to store the squares occupied by each piece 
         # Element 0 should be a list of squares occupied by pawns
         # List continues in PNBRQK format
         occupiedSquares = []
-        for i, pieceColor in enumerate(pieceColors):
-
             # colorPoints holds the points in the image at which each pieceColor occurs
             #colorPoints = board.getBlobPoints(self.board, pieceColors[i])
-            colorPoints = computer_vision.getBlobPoints(self.boardImage, pieceColors[i])
+        colorPoints = computer_vision.getBlobPoints(self.boardImage, whitePieceColor)
 
 
-            # A list to hold all the squares in which a pieceColor occurs
-            colorSquares = []
-            #print(f'colorPoints[{i}]: {colorPoints}')
-            # Iterates across each point where a pieceColor occurs
-            for colorPoint in colorPoints:
-                #print(f'colorPoint: {colorPoint}')
-                # Finds the square that contains that point and appends it to colorSquares
-                square = self.board.getSquare(colorPoint)  
-                colorSquares.append(square)
+        # Iterates across each point where a white blob occurs
+        for colorPoint in colorPoints:
+            #print(f'colorPoint: {colorPoint}')
+            # Finds the square that contains that point and appends it to colorSquares
+            square = self.board.getSquare(colorPoint)  
+            occupiedSquares.append(square)
 
-            # Appends the list of squares occupied by a given color to occupiedSquares
-            occupiedSquares.append(colorSquares)
-        #print(occupiedSquares)
 
-        # Returns a list that contains lists of squares at which each type of piece is located in PNBRQK format 
+        # Returns a list of squares occupied by white pieces.
+        if(len(occupiedSquares) == 0):
+            raise Exception('No white pieces detected')
         return occupiedSquares
 
-# import overhead picture of board
-# getSquare positions only works on the image of the blank chessboard. I could modify it to work with pieces
-# I could also modify it to just take the picture before the pieces are on the board,
-# Simplest solution is simply to draw blobs on the corners of the board. I'll do that
-# board = cv.imread('pictures/game.jpg')
-# cv.imshow("board", board)
 
-# state1 = boardState(board)
-
-# squares1 = state1.getOccupiedSquares()
-# print(squares1)
 
 
